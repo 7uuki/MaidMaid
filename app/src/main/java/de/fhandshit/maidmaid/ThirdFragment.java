195c7071ed8679a0703cb078d8c651f18b3f0d44
Mohammed Alassane <mohammed.alassane@alumni.fh-aachen.de>
2024-04-02 13:07:57 +0200

    Third fragment added, floating actionbutton deleted

---
+++ app/src/main/java/de/fhandshit/maidmaid/ThirdFragment.java
@@ -0,0 +1,9 @@
+package de.fhandshit.maidmaid;
+
+import androidx.fragment.app.Fragment;
+
+import de.fhandshit.maidmaid.databinding.FragmentThirdBinding;
+
+public class ThirdFragment extends Fragment {
+    private FragmentThirdBinding binding;
+}
