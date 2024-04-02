5d5c13b418eaab4b238435f1bd13cabe1f4bd484
HalberBach <nilsjogie@gmail.com>
2024-04-02 15:55:31 +0200

    date

--- app/src/main/java/de/fhandshit/maidmaid/ThirdFragment.java
+++ app/src/main/java/de/fhandshit/maidmaid/ThirdFragment.java
@@ -1,27 +1,52 @@
 package de.fhandshit.maidmaid;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import de.fhandshit.maidmaid.databinding.FragmentSecondBinding;
+import de.fhandshit.maidmaid.databinding.FragmentThirdBinding;
 
 public class ThirdFragment extends Fragment {
+    final Calendar myCalendar = Calendar.getInstance();
+    EditText dateText;
 
-    private FragmentSecondBinding binding;
+    private FragmentThirdBinding binding;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+        dateText = binding.datePicker;
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+            }
+        };
+        dateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,10 +54,12 @@ public class ThirdFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(v ->
-                NavHostFragment.findNavController(ThirdFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment)
-        );
+    }
+
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        dateText.setText(dateFormat.format(myCalendar.getTime()));
     }
 
     @Override
